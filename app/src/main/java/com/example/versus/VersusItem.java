9662626876d2c3e95ecc871c21b119953043a958
Joonyeol Oh <joonohjoon@Joonyeols-MacBook-Pro.local>
2020-06-15 12:00:26 -0400

    Working on local data storage

--- app/src/main/java/com/example/versus/VersusItem.java
+++ app/src/main/java/com/example/versus/VersusItem.java
@@ -1,7 +1,12 @@
 package com.example.versus;
 
+import java.util.ArrayList;
+
 public class VersusItem {
+    private String item_ID;
     private String item_Name;
+    private ArrayList<String> item_Category;
+    private ArrayList<String> item_Value;
 
     public VersusItem(String name) {
         item_Name = name;
